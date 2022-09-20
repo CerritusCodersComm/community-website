44aa37c1d075ee0245776f9f883090a6ff7ded62
ishir <ishiragrawal02@gmail.com>
2022-09-20 09:01:36 +0530

    made the required changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,17 +11,19 @@ export const Hero: FC = () => {
           {" "}
           <div className="divider lg:divider-horizontal"></div>
         </div> */}
-        <div className="absolute ml-[250px] w-1.5 h-3/6 bg-line"></div>
+        <div className="absolute ml-[250px] w-1.5 h-[150px] bg-line"></div>
 
         {/* <div className="border-l-4 border-indigo-500 h-screen"></div> */}
         {/* <h1 className="text-4xl font-bold text-center text-black">Cerritus Coders</h1> */}
-
-        <div className="p-10 flex ">
+        {/* <div className="flex flex-col justify-center"> */}
+        <div className="p-10 flex">
           <Image src={cerrituscollage} height={430} width={558} />
-          <div className="ml-20">
+
+          <div className="ml-[200px]">
             <Image src={cerritusCodersLogo} height={350} width={700} />
           </div>
         </div>
+        {/* </div> */}
         <div className="max-w-3xl mx-auto text-3xl tracking-wide leading-relaxed font-puritan text-center text-black justify-center mb-4">
           This community is run solely by students, for students, to encourage development, open-source, and help the
           community unlock their full potential, one step at a time.
