12fc17e97b13bcf8222680f1174ab170df00c7c1
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-09-27 21:04:21 +0530

    Updated _app.tsx with proper page props

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,25 @@
 import {AppProps} from "next/app";
-import Head from "next/head";
 import {FC} from "react";
 import {AppBar} from "../components/AppBar";
-import {HeroSection} from "../components/HeroSection";
-import {AboutSection} from "../components/AboutSection";
-import {HomeView} from "../views";
 
 require("../styles/globals.css");
 
 const App: FC<AppProps> = ({Component, pageProps}) => {
     return (
         <>
-            <Head>
-                <title>Cerritus Coders</title>
-            </Head>
-            <HomeView />
+            <div className='flex flex-col h-screen'>
+                <div className='relative mb-20'>
+                    <div className='absolute left-0 top-0 background-graphic-upper'></div>
+                    <div className='absolute left-0 right-0 z-20'>
+                        <AppBar/>
+                    </div>
+                </div>
+                <div className='z-10'>
+                    {/* @ts-ignore */}
+                    <Component {...pageProps} />
+                    {/*<Footer/>*/}
+                </div>
+            </div>
         </>
     );
 };
