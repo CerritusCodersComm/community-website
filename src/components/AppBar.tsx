8cf37e070522963e417d4763029f552ab94458fe
Rahilsiddique <siddiquerahil19@gmail.com>
2022-10-20 00:59:05 +0530

    fixed navar responsive issues
    Signed-off-by: Rahilsiddique <siddiquerahil19@gmail.com>

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -60,18 +60,15 @@ export const AppBar: FC = () => {
       <div
         className={
           !nav
-            ? "fixed left-0 top-0 w-[100%] h-screen border-r border-r-grey-900 ease-in-out duration-700 bg-lightPurple"
-            : "fixed left-[-100%] ease-in-out border-r border-r-grey-900 w-[100%] bg-lightPurple duration-700 h-screen"
+            ? "fixed left-0 top-0 w-[100%] min-h-[106vh] border-r border-r-grey-900 ease-in-out duration-700 bg-lightPurple flex justify-center items-center flex-col"
+            : "fixed left-[-100%] ease-in-out border-r border-r-grey-900 w-[100%] bg-lightPurple duration-700 min-h-[106vh]  flex justify-center items-center flex-col"
         }
       >
-        <div className="mt-2 ml-10">
-          <Image src={cerritusCodersLogo} height={107} width={214} />
-        </div>
         <div className="text-2xl flex-shrink-0 pt-0 px-10 flex-cols bg-lightPurple">
           {navLinks.map((link, index) => {
             return (
               <div
-                className="flex justify-center space-x-1 m-2 p-2 cursor-pointer"
+                className="flex justify-center space-x-1 m-2 mt-2 p-2 cursor-pointer"
                 key={index}
                 onClick={() => setNav((prev) => !prev)}
               >
