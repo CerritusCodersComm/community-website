19ec608173968b0bc293c27ee4cf11c69c840fbf
tarunvashishth <tarun.vashishth093@gmail.com>
2022-10-28 16:51:57 +0530

    Made text center

--- src/pages/Errorpage.tsx
+++ src/pages/Errorpage.tsx
@@ -2,7 +2,7 @@ const Errorpage = () => {
   return (
     <>
       <div className="h-screen w-full flex flex-col justify-center items-center bg-[#F1F6F0]">
-        <h1 className="text-4xl text-black tracking-widest">
+        <h1 className="text-4xl text-center text-black tracking-widest">
           Sorry, currently this page is getting built by the community.
         </h1>
       </div>
